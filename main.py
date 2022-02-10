5980a6c360d6c0bf357a0d7043b996b6f26b8972
Jonathan Edwin <jegolvera@gmail.com>
2022-02-10 17:42:36 -0600

    Set image limits

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import PIL.Image
 from openpyxl import Workbook
 import openpyxl
+import os
 
 # Resize Image
 def resize_image(image, new_width=100):
@@ -10,13 +11,14 @@ def resize_image(image, new_width=100):
     resized_image = image.resize((new_width, new_height), PIL.Image.NEAREST)
     return resized_image
 
-def pixel_to_Excelpoint(pixel):
+def pixel_to_Excelpoint(pixel:int):
     return pixel / 7
 
 def pixel_to_excel(image, margenX=0, margenY=0):
     workbook = Workbook()
     sheet = workbook.active
 
+    # Resize the excel sheet
     WIDTH = pixel_to_Excelpoint(20)
     X_LENGTH_PIXEL = image.size[0];
 
@@ -36,11 +38,29 @@ def pixel_to_excel(image, margenX=0, margenY=0):
     workbook.save("draw.xlsx")
 
 
-def main():
-    image = PIL.Image.open("frame2.jpg")
-    image = resize_image(image, 100)
+def draw_image(image_path:str, image_size: int)->None:
+    image = PIL.Image.open(image_path)
+    image = resize_image(image, image_size)
     pixel_to_excel(image)
 
+def main()->None:
+    # Ask user for the image path and size
+    image_path = input("Enter the image path: ")
+
+    # Check if the image exists
+    if not os.path.isfile(image_path):
+        print("The image path is not valid :(")
+        return
+
+    image_size = int(input("Enter the image size to have in Excel: "))
+
+    # Avoid negative values and higher than 1000
+    if image_size <= 0 or image_size > 1000:
+        print("The image size is not valid :(")
+        return
+
+    draw_image(image_path, image_size)
+    print("Image drawn successfully :)")
 
 if __name__ == '__main__':
     main()
